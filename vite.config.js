3eb7d9e0388e94bf07966dc9e8280ef93b30f57b
methzdev <msivminnsbm@gmail.com>
2026-07-25 16:31:18 +0530

    feat: add global styles and theme configuration for auth and jobs layouts

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
     plugins: [react()],
     server: {
-<<<<<<< HEAD
         port: 5173,
-=======
->>>>>>> 4b7e401 (Frontend updates)
         watch: {
             ignored: ['**/.vs/**']
         }
     }
-<<<<<<< HEAD
 });
-=======
-})
->>>>>>> 4b7e401 (Frontend updates)
